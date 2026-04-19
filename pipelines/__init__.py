451b138c8f0d1d2ccc79c0a2fa0db210053e4755
ADONIS <tafadzwameshack01@gmail.com>
2026-04-19 12:03:17 +0200

    Add pipelines/__init__.py

---
+++ pipelines/__init__.py
@@ -0,0 +1,4 @@
+# CONTRACT: pipelines/__init__.py
+from pipelines.result_resolver import ResultResolver
+from pipelines.prematch_pipeline import PreMatchPipeline
+from pipelines.inplay_pipeline import InPlayPipeline
