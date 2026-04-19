a24034dd87234ba644045282d9c6501487be5ceb
ADONIS <tafadzwameshack01@gmail.com>
2026-04-19 12:03:09 +0200

    Add data_sources/__init__.py

---
+++ data_sources/__init__.py
@@ -0,0 +1,9 @@
+# CONTRACT: data_sources/__init__.py
+from data_sources.source_registry import SourceRegistry
+from data_sources.espn_api import ESPNApi
+from data_sources.openligadb import OpenLigaDB
+from data_sources.football_data_org import FootballDataOrg
+from data_sources.api_football import ApiFootball
+from data_sources.understat_scraper import UnderstatScraper
+from data_sources.fbref_scraper import FBrefScraper
+from data_sources.odds_api import OddsApi
